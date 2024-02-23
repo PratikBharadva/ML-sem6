98d560b3428f270cb4002628cc1b534f3387750d
PratikBharadva <124383349+PratikBharadva@users.noreply.github.com>
2024-02-23 12:29:19 +0530

    upto lab4 ML

---
+++ first.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#printing statement
+print("Hello World")
+
+#multiple assignment
+a, b, c = 10, 20, 30
+print(a,b,c)
+print(a)
+
+#dictionary
+mydic = {'a':1, 'b':2, 'c':3}
+print("value of key A:",mydic['a'])
+
+for key in mydic.keys():
+    print(mydic[key])
